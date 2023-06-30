f1dc48f5baef94d4242d575184fb90a74503a874
Sucerquia <dsucerg@gmail.com>
2023-06-30 23:11:43 +0200

    ADD: singles and doubles of this tournament

---
+++ doubles.py
@@ -0,0 +1,12 @@
+from DistributeMatches.distribute import create_tables
+
+
+teams = ["Anthony-Quentin",
+         "Marcus-Valentin",
+         "Harald-Daniel",
+         "Jannik-Saber",
+         "Eric-Leif"]
+
+ngroups = 1
+
+create_tables(teams, ngroups)
---
+++ singles.py
@@ -0,0 +1,21 @@
+from DistributeMatches.distribute import create_tables
+
+
+players = ["Anna",
+           "Anthony",
+           "Kai",
+           "Daniel",
+           "Marcus",
+           "Valentin",
+           "Harald",
+           "Jannik",
+           "Eric",
+           "Leif",
+           "Kiril",
+           "Marc-Oliver",
+           "Saber",
+           "Quentin"]
+
+ngroups = 4
+
+create_tables(players, ngroups)
